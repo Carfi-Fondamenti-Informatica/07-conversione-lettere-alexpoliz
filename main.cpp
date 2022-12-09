b62ed3df0f082f11f2b03dfba3f53a02210d277f
alexpoliz <117836399+alexpoliz@users.noreply.github.com>
2022-12-09 11:14:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+  char n=0; cin >> n;
+  if (funzione(n) == true) {
+    cout << n;
+  } else if (funzione(n)==false){
+    cout << "errore";
+  }
   return 0;
 }
